273fa339dff3186b316ab2dbc6392d74c0330616
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-12-28 20:43:05 -0300

    Curso class

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,42 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String descricao, Integer cargaHoraria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+}
