986d00e62f9640df4156987d4e561500f196e269
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-12-28 22:22:15 -0300

    finish Main

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -16,6 +16,11 @@ public class Main {
         curso.setDescricao("Curso de Java");
         curso.setCargaHoraria(100);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("POO");
+        curso2.setDescricao("Curso de POO");
+        curso2.setCargaHoraria(100);
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria");
         mentoria.setDescricao("Mentoria de Java");
@@ -26,19 +31,42 @@ public class Main {
         bootcamp.setDescricao("Bootcamp de Java");
         bootcamp.getConteudos().add(curso);
         bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(curso2);
 
         Dev dev1 = new Dev();
         dev1.setNome("Dev 1");
         dev1.InscreverBootcamp(bootcamp);
 
-        System.out.println("Conteudos Inscritos" + dev1.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos" + dev1.getConteudosInscritos()+"\n");
+
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+         // dev1.progredir(); // se ativar vai lançar exceção
+
+
+        System.out.println("Conteudos Concluidos" + dev1.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Atualizados" + dev1.getConteudosInscritos());
+        System.out.println("XP: " + dev1.calcularTotalXP());
+
+        System.out.println("-----------------------------------------------------");
+
+
 
         Dev dev2 = new Dev();
         dev2.setNome("Dev 2");
         dev2.InscreverBootcamp(bootcamp);
 
 
-        System.out.println("Conteudos Inscritos" + dev2.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos" + dev2.getConteudosInscritos() + "\n");
+
+        dev2.progredir();
+
+        System.out.println("Conteudos Concluidos" + dev2.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos Atualizados" + dev2.getConteudosInscritos());
+        System.out.println("XP: " + dev2.calcularTotalXP());
+
+
 
 
 
