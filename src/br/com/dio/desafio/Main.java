47805717ce237aba5fb31f0e1afcfe4436e568bb
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-12-28 22:10:31 -0300

    Main basic structure

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -18,6 +20,25 @@ public class Main {
         mentoria.setTitulo("Mentoria");
         mentoria.setDescricao("Mentoria de Java");
         mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Bootcamp de Java");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Dev 1");
+
+        System.out.println("Conteudos Inscritos" + dev1.getConteudosInscritos());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Dev 2");
+
+
+        System.out.println("Conteudos Inscritos" + dev2.getConteudosInscritos());
+
+
 
 
 
